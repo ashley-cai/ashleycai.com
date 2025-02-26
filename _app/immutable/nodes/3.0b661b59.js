8d703e779b9b72389c018645e33b62905a4b8c58
Ashley Cai <ashley.lcai@gmail.com>
2025-02-26 17:15:21 -0500

    Updates

---
+++ _app/immutable/nodes/3.0b661b59.js
@@ -0,0 +1,35 @@
+import{S as de,i as ge,s as he,k as a,a as _,q as o,L as oe,l as r,m as p,c as w,r as i,h as v,M as ie,n as s,N as be,b as ue,G as e,H as re}from"../chunks/index.11c0bfcc.js";import{b as ve}from"../chunks/paths.3bba541b.js";/* empty css                                                        */function pe(ce){let d,b,u,se,I,t,R,D,k,x,S,G,T,E,H,A,L,n,M,N,V,W,z,O,j,q,C,J,U,K,P,Q,F,X,Y,Z,$,ee,te,g,le,h,m;return{c(){d=a("div"),b=a("div"),u=a("img"),I=_(),t=a("div"),R=o(`Hello! I'm Ashley.
+            `),D=a("br"),k=o(`
+            I'm a designer, developer, and journalist with a love for deep diving into topics.
+            `),x=a("br"),S=a("br"),G=o(`
+            I currently study computer science and public policy at Brown University and Graphic Design at the Rhode Island School of Design.
+            `),T=a("br"),E=a("br"),H=o(`
+            In the past, I've worked at:
+            `),A=a("br"),L=_(),n=a("div"),M=o("The Wall Street Journal"),N=a("br"),V=o(`
+                ServiceNow`),W=a("br"),z=o(`
+                The Los Angeles Times`),O=a("br"),j=o(`
+                The Brown Daily Herald`),q=a("br"),C=o(`
+                CDK Global`),J=a("br"),U=o(`
+                +more`),K=_(),P=a("br"),Q=o(`
+            Outside of work, my brain tends to be occupied by friendships, stories, dance, and languages.
+            `),F=a("br"),X=a("br"),Y=o(`
+            I'm always looking for energizing collaborations and new short stories to read, so feel free to reach out!
+            `),Z=a("br"),$=_(),ee=a("br"),te=_(),g=a("a"),le=o(`Download Resume 
+            `),h=oe("svg"),m=oe("path"),this.h()},l(f){d=r(f,"DIV",{class:!0});var y=p(d);b=r(y,"DIV",{class:!0});var B=p(b);u=r(B,"IMG",{loading:!0,class:!0,src:!0,alt:!0}),I=w(B),t=r(B,"DIV",{class:!0});var l=p(t);R=i(l,`Hello! I'm Ashley.
+            `),D=r(l,"BR",{}),k=i(l,`
+            I'm a designer, developer, and journalist with a love for deep diving into topics.
+            `),x=r(l,"BR",{}),S=r(l,"BR",{}),G=i(l,`
+            I currently study computer science and public policy at Brown University and Graphic Design at the Rhode Island School of Design.
+            `),T=r(l,"BR",{}),E=r(l,"BR",{}),H=i(l,`
+            In the past, I've worked at:
+            `),A=r(l,"BR",{}),L=w(l),n=r(l,"DIV",{class:!0});var c=p(n);M=i(c,"The Wall Street Journal"),N=r(c,"BR",{}),V=i(c,`
+                ServiceNow`),W=r(c,"BR",{}),z=i(c,`
+                The Los Angeles Times`),O=r(c,"BR",{}),j=i(c,`
+                The Brown Daily Herald`),q=r(c,"BR",{}),C=i(c,`
+                CDK Global`),J=r(c,"BR",{}),U=i(c,`
+                +more`),c.forEach(v),K=w(l),P=r(l,"BR",{}),Q=i(l,`
+            Outside of work, my brain tends to be occupied by friendships, stories, dance, and languages.
+            `),F=r(l,"BR",{}),X=r(l,"BR",{}),Y=i(l,`
+            I'm always looking for energizing collaborations and new short stories to read, so feel free to reach out!
+            `),Z=r(l,"BR",{}),$=w(l),ee=r(l,"BR",{}),te=w(l),g=r(l,"A",{class:!0,target:!0,href:!0});var ae=p(g);le=i(ae,`Download Resume 
+            `),h=ie(ae,"svg",{class:!0,viewBox:!0,fill:!0,xmlns:!0});var ne=p(h);m=ie(ne,"path",{d:!0,stroke:!0}),p(m).forEach(v),ne.forEach(v),ae.forEach(v),l.forEach(v),B.forEach(v),y.forEach(v),this.h()},h(){s(u,"loading","lazy"),s(u,"class","info-img svelte-1ga8p9e"),be(u.src,se=ve+"/imgs/info/ashley.jpg")||s(u,"src",se),s(u,"alt",""),s(n,"class","light svelte-1ga8p9e"),s(m,"d","M1 19.5L19.5 1M19.5 1H5M19.5 1V15"),s(m,"stroke","black"),s(h,"class","arrow-icon svelte-1ga8p9e"),s(h,"viewBox","0 0 20 20"),s(h,"fill","none"),s(h,"xmlns","http://www.w3.org/2000/svg"),s(g,"class","link svelte-1ga8p9e"),s(g,"target","_blank"),s(g,"href","https://docs.google.com/document/d/1pW_UON6NHvl6m8OzInDWrDWGuJSLm0G6A6uxG9lSbbQ/edit"),s(t,"class","info-text svelte-1ga8p9e"),s(b,"class","info-container svelte-1ga8p9e"),s(d,"class","page-content svelte-1ga8p9e")},m(f,y){ue(f,d,y),e(d,b),e(b,u),e(b,I),e(b,t),e(t,R),e(t,D),e(t,k),e(t,x),e(t,S),e(t,G),e(t,T),e(t,E),e(t,H),e(t,A),e(t,L),e(t,n),e(n,M),e(n,N),e(n,V),e(n,W),e(n,z),e(n,O),e(n,j),e(n,q),e(n,C),e(n,J),e(n,U),e(t,K),e(t,P),e(t,Q),e(t,F),e(t,X),e(t,Y),e(t,Z),e(t,$),e(t,ee),e(t,te),e(t,g),e(g,le),e(g,h),e(h,m)},p:re,i:re,o:re,d(f){f&&v(d)}}}class we extends de{constructor(d){super(),ge(this,d,null,pe,he,{})}}export{we as component};
